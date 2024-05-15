d5195d0eebe29d419c06cffcbc6ee05560502d1b
monsieurjonees <phil.reitzjones@me.com>
2024-05-15 18:21:21 -0500

    More tinkering and code cleanup

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+// We can hopefully reuse some of these in the final product (hence the docs)
+
 /**
  * Registers the given closure/function as an event listener for 
  * "DOMContentLoaded", thus running it only once all content has loaded.
