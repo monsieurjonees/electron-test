22233f21cb8860e53c5bdec19e8ca9a3b07c30cc
monsieurjonees <phil.reitzjones@me.com>
2024-05-15 12:05:32 -0500

    Tabs kinda working
    Co-authored-by: Kaliana Andriamananjara <kandriam@users.noreply.github.com>

--- src/renderer.js
+++ src/renderer.js
@@ -8,3 +8,14 @@ function sendNotif() {
 function helloClick() {
     window.ipc.helloClick()
 }
+
+function openTab(tab) {
+    var tabs = document.getElementsByClassName("Tab")
+    for (let i = 0; i < tabs.length; i++) {
+        const element = tabs[i];
+        element.style.display = "none"
+    }
+
+    var tabElement = document.getElementById(tab)
+    tabElement.style.display = "block"
+}
