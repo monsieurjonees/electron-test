6cb9440018cd55af83fbf2f4a6af09bae293beb6
monsieurjonees <phil.reitzjones@me.com>
2024-05-15 15:21:33 -0500

    delete electron remnants

--- electron/preload.js
+++
@@ -1,8 +0,0 @@
-// See the Electron documentation for details on how to use preload scripts:
-// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
-const { contextBridge, ipcRenderer } = require('electron')
-
-contextBridge.exposeInMainWorld('ipc', {
-    sendNotif: () => {ipcRenderer.send("send-notif-pls")},
-    helloClick: () => {ipcRenderer.send("hello")}
-})
