071986c43e8e9c1febae3413c4396ed2e5a0e79a
monsieurjonees <phil.reitzjones@me.com>
2024-05-17 12:59:23 -0500

    some index cleanup

--- src/index.js
+++ src/index.js
@@ -12,25 +12,8 @@ function openTab(event) {
 
     var tabLink = document.getElementsByClassName("TabButton active")[0]
     tabLink.className = tabLink.className.replace(" active", "")
-    // if (tabLink.className == "TabButton special") {
-    //     var elements = document.getElementsByClassName("SpecialSheet")
-    //     for (let i = 0; i < elements.length; i++) {
-    //         const element = elements[i];
-    //         document.head.removeChild(element)
-    //     }
-    // }
 
     var tabElement = document.getElementById(event.currentTarget.name)
-    // if (event.currentTarget.className == "TabButton special") {
-    //     console.log("h")
-    //     var head = document.head
-    //     var newStyle = document.createElement("link")
-    //     newStyle.type = "text/css"
-    //     newStyle.rel = "stylesheet"
-    //     newStyle.className = "SpecialSheet"
-    //     newStyle.href = "/src/special.css"
-    //     head.appendChild(newStyle)
-    // }
     tabElement.style.display = "block"
     event.currentTarget.className += " active"
 }
